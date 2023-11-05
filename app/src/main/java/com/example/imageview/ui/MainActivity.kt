bc013751cedc16bca3b5cbf3fffdf8cafc2c1fdd
Mohamed Naser <mahmmedn19@gmail.com>
2023-11-05 15:40:31 +0200

    refactor pkg

--- app/src/main/java/com/example/imageview/ui/MainActivity.kt
+++ app/src/main/java/com/example/imageview/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.imageview
+package com.example.imageview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.imageview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
